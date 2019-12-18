c394822a2b170810a217d5ef6a19f888f32ca8c8
stupidHIGH <52411066+stupidHIGH@users.noreply.github.com>
2019-12-18 18:48:34 +0800

    Update get_DIY_vectors.py
    right wrongs

--- word_vectors/DIY_word_vector/get_DIY_vectors.py
+++ word_vectors/DIY_word_vector/get_DIY_vectors.py
@@ -3,22 +3,28 @@ import gensim
 import logging
 import sys
 import os
-
+import numpy as np
 if __name__=="__main__":
     save_model_name = sys.path[0]+'\my_diy_language_model'
 
     # 加载已训练好的模型
-    model_1 = word2vec.Word2Vec.load(save_model_name)
-    
+    language_model = word2vec.Word2Vec.load(save_model_name)
+    #--------------------------------------------------------------------------------------------------------
+    # 得到当前文本text的词向量矩阵  text=[sentence,sentence,sentence...],sentence=[word,word,word,...]  
+    # word为单个字符串 & 每个sentence要事先处理为等长
+    for sentence in text:
+        sentence_vec=[]
+        for word in sentence:
+            sentence_vec.append(language_model[word])
+        text_vec.append(sentence_vec)
+    matrix=np.array(text_vec)
+    #--------------------------------------------------------------------------------------------------
     # 计算两个词的相似度/相关程度
-    y1 = model_1.similarity("计算机", "科学")
-    print(y1)
-    y1 = model_1.similarity("计算机", "飞机")
-    print(y1)
-
+    print(language_model.similarity("计算机", "科学"))
+    print(language_model.similarity("计算机", "飞机"))
+    #--------------------------------------------------------------------------------------------------
     # 计算某个词的相关词列表
-    y2 = model_1.most_similar("计算机", topn=10)  # 10个最相关的
+    y2 = language_model.most_similar("计算机", topn=10)  # 10个最相关的
     print(u"和计算机最相关的词有：\n")
-    for item in y2:
-        print(item[0], item[1])
-    print("-------------------------------\n")
+    for each in y2:
+        print(each[0], each[1])
