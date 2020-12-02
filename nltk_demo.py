af687e5a38d1f55f8a13501bb111be8f63b5bdd8
stupidHIGH <1370215371@qq.com>
2020-12-02 16:42:39 +0800

    Create nltk_demo.py
    nltk基本用法demo

---
+++ nltk_demo.py
@@ -0,0 +1,37 @@
+import nltk
+
+#寻找同义、反义词
+from nltk.corpus import wordnet
+syns = wordnet.synsets("happy")
+synonyms, antonyms= [], []
+for each in syns:
+    for l in each.lemmas():
+        print(l)
+        synonyms.append(l.name())
+        if l.antonyms():
+            antonyms.append(l.antonyms()[0].name())
+print(antonyms) # 反义词
+print(synonyms) # 同义词
+
+#分词、分句
+from nltk.tokenize import word_tokenize, sent_tokenize
+text = "I want to eat 8 sandwitchs!!! Because I'm hungry"
+words = word_tokenize(text)
+sents = sent_tokenize(text)
+
+#输出n-grams
+print(list(nltk.bigrams(words)))
+print(list(nltk.trigrams(words)))
+print(list(nltk.ngrams(words,4,pad_left=True,pad_right=False))) # 4-grams
+
+#绘制词频分布图
+fd = nltk.FreqDist(words)
+fd.plot()
+
+#寻找词根
+from nltk.stem import WordNetLemmatizer
+words = ["run","ran","running","runs"]
+wnl = WordNetLemmatizer()
+for word in words:
+    root_word = wnl.lemmatize(word)
+    print(root_word)
